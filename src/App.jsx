b001e6108eaee414ff9d6a75815b3c1ab4cee4f5
ahaddev655 <ahad.dev.eng@gmail.com>
2026-01-07 19:22:54 +0500

    Admin Panel deleted

--- src/App.jsx
+++ src/App.jsx
@@ -8,10 +8,6 @@ import MainPortfolioPage from "./pages/main/MainPortfolioPage";
 import MainMarketPage from "./pages/main/MainMarketPage";
 import MainSearchPage from "./pages/main/MainSearchPage";
 import MainSettingsPage from "./pages/main/MainSettingsPage";
-import AdminLayout from "./layouts/AdminLayout";
-import AdminUsers from "./pages/admin/AdminUsers";
-import AdminDashboard from "./pages/admin/AdminDashboard";
-import AdminSettingsPage from "./pages/admin/AdminSettingsPage";
 import MainCoinPage from "./pages/main/MainCoinPage";
 import MainCoinsHoldingHistory from "./pages/main/MainCoinsHoldingHistory";
 
@@ -48,24 +44,6 @@ function App() {
         {
           path: "coins-history",
           element: <MainCoinsHoldingHistory />,
-        },
-      ],
-    },
-    {
-      path: "/66e5753c/",
-      element: <AdminLayout />,
-      children: [
-        {
-          index: true,
-          element: <AdminDashboard />,
-        },
-        {
-          path: "users",
-          element: <AdminUsers />,
-        },
-        {
-          path: "settings",
-          element: <AdminSettingsPage />,
         },
       ],
     },
